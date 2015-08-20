8bf9862ba001a712d7e3906af93828b61690594c
Terry Shannon <tgs154-bw@yahoo.com>
2015-08-19 21:48:27 -0400

    clean up comments for 8 space tabs

--- cachematrix.R
+++ cachematrix.R
@@ -7,13 +7,13 @@
 ## Use: cMatrix <- makeCacheMatrix(x)
 ## parameter: x - square matrix
 makeCacheMatrix <- function(x = matrix()) {
-	inv <- NULL						# create a cache store for invest matrix
+	inv <- NULL				# create a cache store for invest matrix
 	set <- function(y) {			# function to set value of cache store matrix
-		x <<- y						# set matrix cache to input parameter
-		inv <<- NULL				# initialize cache store for inverse
+		x <<- y				# set matrix cache to input parameter
+		inv <<- NULL			# initialize cache store for inverse
 	}
-	get <- function() x				# return cached matrix
-	setinv <- function(i) inv <<- i # set inverse matrix to input parameter
+	get <- function() x			# return cached matrix
+	setinv <- function(i) inv <<- i 	# set inverse matrix to input parameter
 	getinv <- function() inv		# return cached matrix inverse
 	list(set = set, get = get, setinv = setinv, getinv = getinv)	
 }
@@ -26,13 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## TEST: x_inv %*% x$get() should return identity matrix
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-	inv <- x$getinv()				# get current cached inverse matrix
+	inv <- x$getinv()			# get current cached inverse matrix
 	if (!is.null(inv)) {			# if one exists return from cache
 		message("getting cached data")
-		return(inv)					# return inverse matrix for cached matrix
+		return(inv)			# return inverse matrix for cached matrix
 	}
-	data <- x$get()					# get original matrix from cache
-	inv <- solve(data)				# calculate inverse of cached matrix
-	x$setinv(inv)					# set cache inverse matrix
-	inv								# return cached matrix inverse
+	data <- x$get()				# get original matrix from cache
+	inv <- solve(data)			# calculate inverse of cached matrix
+	x$setinv(inv)				# set cache inverse matrix
+	inv					# return cached matrix inverse
 }
